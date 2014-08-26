cffa7afb08197442b75b3c771c41330c0d69a261
mmcomp <m.mirsamie@gmail.com>
2014-08-26 11:11:40 +0430

    میرسمیع : نمونه ۵ مدل استفاده از این سیستم به همراه نوع مبلغ

--- numberMask.js
+++ numberMask.js
@@ -1,10 +1,80 @@
 var tmp_val='';
+function underThous(inpp)
+{
+	var out = '';
+	var addad_small = {1:'یک',2:'دو',3:'سه',4:'چهار',5:'پنج',6:'شش',7:'هفت',8:'هشت',9:'نه'};
+	var addad_middle = {10:'ده',11:'یازده',12:'دوازده',13:'سیزده',14:'چهارده',15:'پانزده',16:'شانزده',17:'هفده',18:'هجده',19:'نوزده',20:'بیست',30:'سی',40:'چهل',50:'پنجاه',60:'شصت',70:'هفتاد',80:'هشتاد',90:'نود',100:'صد'};
+	var addad_large = {100:'صد',200:'دویست',300:'سیصد',400:'چهارصد',500:'پانصد',600:'ششصد',700:'هفتصد',800:'هشتصد',900:'نهصد',1000:'هزار'};
+	if(inpp < 1000)
+	{
+		var out_arr = [];
+		var kharej = inpp - (inpp % 100);
+		if(typeof addad_large[kharej] != 'undefined')
+			out_arr.push(addad_large[kharej]);
+		inpp -= kharej;
+		if(inpp > 20)
+			var kharej = inpp - (inpp % 10);
+		else
+			var kharej = inpp;
+		if(typeof addad_middle[kharej] != 'undefined')
+			out_arr.push(addad_middle[kharej]);
+		if(inpp > 20 || inpp < 10)
+		{
+			if(inpp > 20)
+				inpp -= kharej;
+			if(typeof addad_small[inpp] != 'undefined')
+				out_arr.push(addad_small[inpp]);
+		}
+		out = out_arr.join(' و ');
+	}
+	return(out);
+}
+function addad(inp)
+{
+	var addad_level = ['','هزار','میلیون','میلیارد','تریلیارد'];
+	var out = '';
+	var inp = parseInt(inp,10);
+	if(typeof inp != 'undefined' && !isNaN(inp))
+	{
+		var maxLevel = 0;
+		var maxLevelFound = false;
+		for(var i = 0 ; i < 5 && !maxLevelFound; i++)
+		{
+			var kharej = parseInt(inp/Math.pow(10,i*3),10);
+			if(kharej == 0)
+			{
+				maxLevel = (i>0)?i-1:0;
+				maxLevelFound = true;
+			}
+		}
+		var inp_tmp = inp;
+		var out_arr = [];
+		for(var i = maxLevel; i >= 0; i--)
+		{
+			var kharej = parseInt(inp_tmp/Math.pow(10,i*3),10);
+			inp_tmp -= kharej*Math.pow(10,i*3);
+			out_arr.push(underThous(kharej)+' '+addad_level[i]);
+		}
+		out = out_arr.join(' و ');
+	}
+	return(out);
+}
+function formatMoney(n,c,d,t){
+	var c = isNaN(c = Math.abs(c)) ? 2 : c,
+	d = d == undefined ? "." : d,
+	t = t == undefined ? "," : t,
+	s = n < 0 ? "-" : "",
+	i = parseInt(n = Math.abs(+n || 0).toFixed(c)) + "",
+	j = (j = i.length) > 3 ? j % 3 : 0;
+	return s + (j ? i.substr(0, j) + t : "") + i.substr(j).replace(/(\d{3})(?=\d)/g, "$1" + t) + (c ? d + Math.abs(n - i).toFixed(c).slice(2) : "");
+}
 function killNoneNumber(e)
 {
 	e = e || window.event;
 	var charCode = (typeof e.which == "undefined") ? e.keyCode : e.which;
 	var charStr = String.fromCharCode(charCode);
-	if (!/\d/.test(charStr) && !e.altKey && !e.ctrlKey) {
+	var conf = fetchConfig(e.target.title);
+	if (!/\d/.test(charStr) && !e.altKey && !e.ctrlKey && charStr!=conf.block_sep) {
 		return false;
 	}
 	if(validateInput(e.target))
@@ -16,17 +86,32 @@ function fetchConfig(conf)
 {
 	var blocks = [];
 	var block_sep = '';
-	var inp = conf.replace(/#/g,'');
-	var inp = inp.replace(/@/g,'');
-	block_sep = inp.split('')[0];
-	var tmp = conf.split(block_sep);
-	blocks = [];
-	for(var i = 0;i < tmp.length;i++)
-		blocks.push((tmp[i]!='@')?tmp[i].length:0);
+	if(conf == 'C' || conf == 'c')
+	{
+		blocks = [((conf=='C')?-1:-2)];
+		block_sep = ',';
+	}
+	else
+	{
+		var inp = conf.replace(/#/g,'');
+		var inp = inp.replace(/@/g,'');
+		block_sep = inp.split('')[0];
+		var tmp = conf.split(block_sep);
+		blocks = [];
+		for(var i = 0;i < tmp.length;i++)
+			blocks.push((tmp[i]!='@')?tmp[i].length:0);
+	}
 	return({
 		"blocks" : blocks,
 		"block_sep" : block_sep
 	});
+}
+function removeHoroof(matchClass)
+{
+	var elems = document.getElementsByTagName('*'), i;
+	for(i in elems)
+		if((' ' + elems[i].className + ' ').indexOf(' ' + matchClass + ' ')> -1)
+			elems[i].parentElement.removeChild(elems[i]);
 }
 function checkInputForNumber(obj,conf)
 {
@@ -35,20 +120,44 @@ function checkInputForNumber(obj,conf)
 	var out = false;
 	if(obj)
 	{
-		var val = obj.value;
-		var tmp = val.split(block_sep);
-		if(tmp.length == blocks.length)
+		if(blocks[0] < 0)
 		{
+			var val = obj.value;
+			var tmp = val.replace(/,/g , '');
 			out = true;
-			for(var i = 0;i < tmp.length;i++)
+			obj.value = formatMoney(tmp,0, '.', ',');
+			if(blocks[0]==-1)
 			{
-				var tmp_num = parseInt(tmp[i],10);
-				if(isNaN(tmp_num))
-					out = false;
-				else
-					if(blocks[i] > 0)
-						if(tmp[i].length > blocks[i])
-							out = false;
+				var parentGuest = obj;
+				var childGuest = document.createElement("span");
+				childGuest.className = "adad_horoof";
+				childGuest.innerHTML = addad(tmp);
+				removeHoroof("adad_horoof");
+				if (parentGuest.nextSibling) {
+				  parentGuest.parentNode.insertBefore(childGuest, parentGuest.nextSibling);
+				}
+				else {
+				  parentGuest.parentNode.appendChild(childGuest);
+				}
+			}
+		}
+		else
+		{
+			var val = obj.value;
+			var tmp = val.split(block_sep);
+			if(tmp.length <= blocks.length)
+			{
+				out = true;
+				for(var i = 0;i < tmp.length;i++)
+				{
+					var tmp_num = parseInt(tmp[i],10);
+//					if(isNaN(tmp_num))
+//						out = false;
+//					else
+						if(blocks[i] > 0)
+							if(tmp[i].length > blocks[i])
+								out = false;
+				}
 			}
 		}
 	}
